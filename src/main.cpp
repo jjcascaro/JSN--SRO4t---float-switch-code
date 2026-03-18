41fd7aa13eadd792899777f6f0a3d3d450cb0efe
jjcascaro <jjcascaro@yahoo.com>
2026-03-18 11:57:10 +0800

    new system

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #define TRIG_PIN D1  // GPIO13 - Trigger
 #define ECHO_PIN D2  // GPIO15 - Echo
 // Float switch pin
-#define FLOAT_SWITCH_PIN D0  // GPIO16 - Float switch
+#define FLOAT_SWITCH_PIN D5  // GPIO14 - Float switch (D0/GPIO16 does not support INPUT_PULLUP)
 
 float distance = 0;
 unsigned long lastTime = 0;
@@ -16,7 +16,7 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   digitalWrite(TRIG_PIN, LOW);
-  pinMode(FLOAT_SWITCH_PIN, INPUT); // Set float switch pin as input
+  pinMode(FLOAT_SWITCH_PIN, INPUT_PULLUP); // Internal pull-up: LOW = switch closed, HIGH = switch open
 }
 
 float measureDistance() {
@@ -38,7 +38,7 @@ float measureDistance() {
 }
 
 void loop() {
-  if (millis() - lastTime >= 10000) { // Measure every 10 seconds
+  if (millis() - lastTime >= 1000) { // Measure every 1 seconds
     lastTime = millis();
     
     distance = measureDistance();
@@ -47,9 +47,9 @@ void loop() {
     Serial.print(distance, 1);
     Serial.println(" cm");
     
-    // Float switch sensor reading
+    // Float switch sensor reading (INPUT_PULLUP: LOW = closed/HIGH water, HIGH = open/LOW water)
     int sensorState = digitalRead(FLOAT_SWITCH_PIN);
-    if (sensorState == HIGH) {
+    if (sensorState == LOW) {
       Serial.println("Water level: HIGH (Switch Closed)");
     } else {
       Serial.println("Water level: LOW (Switch Open)");
